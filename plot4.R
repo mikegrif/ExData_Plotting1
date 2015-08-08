19e2a531f323361f7bd44f01293f8ac555f3c307
mikegrif <mikegrif@bellsouth.net>
2015-08-08 12:18:19 -0400

    initial checkin

---
+++ plot4.R
@@ -0,0 +1,55 @@
+"
+This script uses individual household electric power consumption data
+from the UC Irvine Machine Learning Repository. The data contains
+measurement of electric power consumption in one household with a one
+minute sampling rate over a period of 4 years.
+
+The script will use data from 2/1/2007 to 2/2/2007 to create
+a histogram of global active power.
+"
+require("dplyr")
+require("lubridate")
+
+drawPlot4 <- function() {
+    
+    if (is.null(power_df)) {
+        message("Reading data from file...")
+        power_df <- read.csv("data/household_power_consumption.txt",header=TRUE,
+                         sep=";", na.strings="?",stringsAsFactors=FALSE)
+    }
+    else {
+        message("Using cached data")
+    }
+    # create new column "timestamp" based on combination of "Date" and "Time" columns
+    df <- mutate(power_df,timestamp=parse_date_time(paste(Date,Time), orders="dmy hms"))
+    
+    # remove Date and Time columns since they are no longer needed
+    df <- select(df, -c(Date,Time))
+    
+    # extract data from 2/1/2007 to 2/2/2007 and save it
+    start <- as.POSIXct("2007-01-31 23:59:59", tz="GMT")
+    end <- as.POSIXct("2007-02-03 00:00:00",tz="GMT")
+    feb2007 <- filter(df,timestamp > start, timestamp < end)
+    
+    # save plot to PNG file
+    y1 <- feb2007$Sub_metering_1
+    y2 <- feb2007$Sub_metering_2
+    y3 <- feb2007$Sub_metering_3
+    
+    # save plots to file
+    png(file="data/plot4.png",width=480,height=480)
+    par(mfrow = c(2,2))
+    with(feb2007,{
+        plot(timestamp,Global_active_power,type='l',ylab="Global Active Power (kilowatts)",xlab="") 
+        plot(timestamp,Voltage,type="l",ylab="Voltage",xlab="datetime")
+        plot(timestamp,Sub_metering_1,type='l',ylab='Energy sub metering',xlab='')
+        lines(timestamp,Sub_metering_2,col='red')
+        lines(timestamp,Sub_metering_3,col='blue')
+        colors <- c("black","red","blue")
+        labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+        legend("topright",legend=labels,col=colors,lty=c(1,1))
+        plot(timestamp,Global_reactive_power,type='l',xlab="datetime")
+    })
+         
+    dev.off()
+}
